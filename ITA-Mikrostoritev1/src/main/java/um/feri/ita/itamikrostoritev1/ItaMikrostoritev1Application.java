33a1efe7e19991eaf14d6cc144f69d913804e27a
leitgebgal <79462408+leitgebgal@users.noreply.github.com>
2024-03-10 23:10:30 +0100

    Create Springboot project

---
+++ ITA-Mikrostoritev1/src/main/java/um/feri/ita/itamikrostoritev1/ItaMikrostoritev1Application.java
@@ -0,0 +1,13 @@
+package um.feri.ita.itamikrostoritev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaMikrostoritev1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItaMikrostoritev1Application.class, args);
+    }
+
+}
---
+++ ITA-Mikrostoritev1/src/test/java/um/feri/ita/itamikrostoritev1/ItaMikrostoritev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package um.feri.ita.itamikrostoritev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItaMikrostoritev1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
