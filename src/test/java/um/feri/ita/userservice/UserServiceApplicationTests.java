bb4e04907c67e35e4eacf55693c4a76e0e10f18b
Gal Leitgeb <gal.leitgeb@gmail.com>
2024-05-28 23:43:46 +0200

    fix

--- src/test/java/um/feri/ita/userservice/UserServiceApplicationTests.java
+++ src/test/java/um/feri/ita/userservice/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package um.feri.ita.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.data.mongodb.uri=mongodb://admin:admin@localhost:27018/?authSource=admin&retryWrites=true&w=majority&appName=ita")
 class UserServiceApplicationTests {
 
     @Test
