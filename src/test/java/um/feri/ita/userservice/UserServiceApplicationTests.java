d35c1b7956f6f510b4a63c9153dd4362215ab387
Gal Leitgeb <gal.leitgeb@gmail.com>
2024-06-19 22:30:08 +0200

    add appp-test.properties

--- src/test/java/um/feri/ita/userservice/UserServiceApplicationTests.java
+++ src/test/java/um/feri/ita/userservice/UserServiceApplicationTests.java
@@ -2,9 +2,10 @@ package um.feri.ita.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
 
     @Test
